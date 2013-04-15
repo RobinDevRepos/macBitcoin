d7ab1926dd4bb7959ef8f7f94d1001aff2c3c1f7
Myles Grant <myles@mylesgrant.com>
2013-04-14 22:45:04 -0700

    Convert VarInt to use NSData+Integer

--- MacBitCoin/BitcoinVarInt.m
+++ MacBitCoin/BitcoinVarInt.m
@@ -33,9 +33,7 @@
 // TODO: Turn this conversion code into general byte array conversion code
 -(id)initFromBytes:(NSData *)data fromOffset:(int)offset{
 	if ((self = [super init])){
-		uint8_t *buf = (uint8_t *)[data bytes];
-		
-		uint8_t first = buf[offset++];
+		uint8_t first = [data offsetToInt8:offset];
 		if (first < 253) {
 			// 8 bits.
 			_value = first;
@@ -43,28 +41,17 @@
 		}
 		else if (first == 253) {
 			// 16 bits.
-			_value = buf[offset++]
-				| ((uint16_t)buf[offset] << 8 );
+			_value = [data offsetToInt16:offset+1];
 			_size = 3;
 		}
 		else if (first == 254) {
 			// 32 bits.
-			_value = buf[offset++]
-				| ((uint16_t)buf[offset++] << 8 )
-				| ((uint32_t)buf[offset++] << 16)
-				| ((uint32_t)buf[offset]   << 24);
+			_value = [data offsetToInt32:offset+1];
 			_size = 5;
 		}
 		else {
 			// 64 bits.
-			_value = buf[offset++]
-				| ((uint16_t)buf[offset++] << 8 )
-				| ((uint32_t)buf[offset++] << 16)
-				| ((uint32_t)buf[offset++] << 24)
-				| ((uint64_t)buf[offset++] << 32)
-				| ((uint64_t)buf[offset++] << 40)
-				| ((uint64_t)buf[offset++] << 48)
-				| ((uint64_t)buf[offset]   << 56);
+			_value = [data offsetToInt64:offset+1];
 			_size = 9;
 		}
 	}
@@ -86,38 +73,23 @@
 // Returns the NSData byte representation of a varInt, like for writing over the network
 // TODO: Turn this conversion code into general byte array conversion code
 -(NSData*) getData{
-	NSMutableData *data;
-	unsigned char buffer[9];
+	NSMutableData *data = [NSMutableData data];
+	
 	switch (self.size) {
 		case 1:
-			buffer[0] = (uint8_t)self.value;
-			data = [NSData dataWithBytes:buffer length:1];
+			[data appendData:[NSData dataWithInt8:(uint8_t)self.value]];
 			break;
 		case 3:
-			buffer[0] = 253; // 0xfd
-			buffer[1] = (uint8_t)self.value;
-			buffer[2] = (uint8_t)(self.value >> 8);
-			data = [NSData dataWithBytes:buffer length:3];
+			[data appendData:[NSData dataWithInt8:253]];
+			[data appendData:[NSData dataWithInt16:(uint16_t)self.value]];
 			break;
 		case 5:
-			buffer[0] = 254; // 0xfe
-			buffer[1] = (uint8_t)self.value;
-			buffer[2] = (uint8_t)(self.value >> 8 );
-			buffer[3] = (uint8_t)(self.value >> 16);
-			buffer[4] = (uint8_t)(self.value >> 24);
-			data = [NSData dataWithBytes:buffer length:5];
+			[data appendData:[NSData dataWithInt8:254]];
+			[data appendData:[NSData dataWithInt32:(uint32_t)self.value]];
 			break;
 		case 9:
-			buffer[0] = 255; // 0xff
-			buffer[1] = (uint8_t)self.value;
-			buffer[2] = (uint8_t)(self.value >> 8 );
-			buffer[3] = (uint8_t)(self.value >> 16);
-			buffer[4] = (uint8_t)(self.value >> 24);
-			buffer[5] = (uint8_t)(self.value >> 32);
-			buffer[6] = (uint8_t)(self.value >> 40);
-			buffer[7] = (uint8_t)(self.value >> 48);
-			buffer[8] = (uint8_t)(self.value >> 56);
-			data = [NSData dataWithBytes:buffer length:9];
+			[data appendData:[NSData dataWithInt8:255]];
+			[data appendData:[NSData dataWithInt64:self.value]];
 			break;
 	}
 	
