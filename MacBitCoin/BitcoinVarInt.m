6476c4c32a9150a2be6419576962b3a0f8e248a5
Myles Grant <myles@mylesgrant.com>
2013-04-14 18:04:23 -0700

    New 16 bit varInt test and fixes for the bug it found

--- MacBitCoin/BitcoinVarInt.m
+++ MacBitCoin/BitcoinVarInt.m
@@ -24,9 +24,9 @@
 // TODO: Turn this conversion code into general byte array conversion code
 -(id)initFromBytes:(NSData *)data fromOffset:(int)offset{
 	if ((self = [super init])){
-		uint64_t *buf = (uint64_t *)[data bytes];
+		uint8_t *buf = (uint8_t *)[data bytes];
 		
-		uint8_t first = (uint8_t)buf[offset];
+		uint8_t first = buf[offset];
 		if (first < 253) {
 			// 8 bits.
 			self.value = first;
@@ -34,31 +34,28 @@
 		}
 		else if (first == 253) {
 			// 16 bits.
-			self.value =
-				((uint16_t)buf[offset + 1] << 0 ) |
-				((uint16_t)buf[offset + 2] << 8 );
+			self.value = buf[offset + 1]
+				| ((uint16_t)buf[offset + 2] << 8 );
 			self.size = 3;
 		}
 		else if (first == 254) {
 			// 32 bits.
-			self.value =
-				((uint16_t)buf[offset + 1] << 0 ) |
-				((uint16_t)buf[offset + 2] << 8 ) |
-				((uint32_t)buf[offset + 3] << 16) |
-				((uint32_t)buf[offset + 4] << 24);
+			self.value = buf[offset + 1]
+				| ((uint16_t)buf[offset + 2] << 8 )
+				| ((uint32_t)buf[offset + 3] << 16)
+				| ((uint32_t)buf[offset + 4] << 24);
 			self.size = 5;
 		}
 		else {
 			// 64 bits.
-			self.value =
-				((uint16_t)buf[offset + 1] << 0 ) |
-				((uint16_t)buf[offset + 2] << 8 ) |
-				((uint32_t)buf[offset + 3] << 16) |
-				((uint32_t)buf[offset + 4] << 24) |
-				((uint64_t)buf[offset + 5] << 32) |
-				((uint64_t)buf[offset + 6] << 40) |
-				((uint64_t)buf[offset + 7] << 48) |
-				((uint64_t)buf[offset + 8] << 56);
+			self.value = buf[offset + 1]
+				| ((uint16_t)buf[offset + 2] << 8 )
+				| ((uint32_t)buf[offset + 3] << 16)
+				| ((uint32_t)buf[offset + 4] << 24)
+				| ((uint64_t)buf[offset + 5] << 32)
+				| ((uint64_t)buf[offset + 6] << 40)
+				| ((uint64_t)buf[offset + 7] << 48)
+				| ((uint64_t)buf[offset + 8] << 56);
 			self.size = 9;
 		}
 	}
@@ -84,33 +81,33 @@
 	unsigned char buffer[9];
 	switch (self.size) {
 		case 1:
-			buffer[0] = self.value;
+			buffer[0] = (uint8_t)self.value;
 			data = [NSData dataWithBytes:buffer length:1];
 			break;
 		case 3:
 			buffer[0] = 253; // 0xfd
-			buffer[1] = (uint16_t)self.value;
-			buffer[2] = (uint16_t)self.value >> 8;
+			buffer[1] = (uint8_t)self.value;
+			buffer[2] = (uint8_t)(self.value >> 8);
 			data = [NSData dataWithBytes:buffer length:3];
 			break;
 		case 5:
 			buffer[0] = 254; // 0xfe
-			buffer[1] = (uint32_t)self.value;
-			buffer[2] = (uint32_t)self.value >> 8;
-			buffer[3] = (uint32_t)self.value >> 16;
-			buffer[4] = (uint32_t)self.value >> 24;
+			buffer[1] = (uint8_t)self.value;
+			buffer[2] = (uint8_t)(self.value >> 8 );
+			buffer[3] = (uint8_t)(self.value >> 16);
+			buffer[4] = (uint8_t)(self.value >> 24);
 			data = [NSData dataWithBytes:buffer length:5];
 			break;
 		case 9:
 			buffer[0] = 255; // 0xff
-			buffer[1] = self.value;
-			buffer[2] = self.value >> 8;
-			buffer[3] = self.value >> 16;
-			buffer[4] = self.value >> 24;
-			buffer[5] = self.value >> 32;
-			buffer[6] = self.value >> 40;
-			buffer[7] = self.value >> 48;
-			buffer[8] = self.value >> 56;
+			buffer[1] = (uint8_t)self.value;
+			buffer[2] = (uint8_t)(self.value >> 8 );
+			buffer[3] = (uint8_t)(self.value >> 16);
+			buffer[4] = (uint8_t)(self.value >> 24);
+			buffer[5] = (uint8_t)(self.value >> 32);
+			buffer[6] = (uint8_t)(self.value >> 40);
+			buffer[7] = (uint8_t)(self.value >> 48);
+			buffer[8] = (uint8_t)(self.value >> 56);
 			data = [NSData dataWithBytes:buffer length:9];
 			break;
 	}
--- MacBitCoinTests/MacBitCoinTests.m
+++ MacBitCoinTests/MacBitCoinTests.m
@@ -26,24 +26,42 @@
     [super tearDown];
 }
 
-- (void)testVarInt
+- (void)testVarInt8Bit
 {
 	uint64_t value = 15;
 	uint8_t size = 1;
 	
     BitcoinVarInt *varInt1 = [[BitcoinVarInt alloc] initFromValue:value];
-	STAssertEquals(varInt1.value, value, @"Value1 does not match");
-	STAssertEquals(varInt1.size, size, @"Size1 does not match");
+	STAssertEquals(varInt1.value, value, @"8bit value1 does not match");
+	STAssertEquals(varInt1.size, size, @"8bit size1 does not match");
 	
-	const char bytes[] = { value };
-	NSData *data = [NSData dataWithBytes:bytes length:size];
+	const char bytes[] = { 0x0f };
+	NSData *data = [NSData dataWithBytes:bytes length:sizeof(bytes)];
 	BitcoinVarInt *varInt2 = [[BitcoinVarInt alloc] initFromBytes:data fromOffset:0];
-	STAssertEquals(varInt2.value, value, @"Value2 does not match");
-	STAssertEquals(varInt2.size, size, @"Size2 does not match");
+	STAssertEquals(varInt2.value, value, @"8bit value2 does not match");
+	STAssertEquals(varInt2.size, size, @"8bit size2 does not match");
 
 	NSData *dataFromVarInt = [varInt1 getData];
-	STAssertEqualObjects(dataFromVarInt, data, @"getData does not match");
+	STAssertEqualObjects(dataFromVarInt, data, @"8bit getData does not match");
+}
 
+- (void)testVarInt16Bit
+{
+	uint64_t value = 65535;
+	uint8_t size = 3;
+	
+    BitcoinVarInt *varInt1 = [[BitcoinVarInt alloc] initFromValue:value];
+	STAssertEquals(varInt1.value, value, @"16bit value1 does not match");
+	STAssertEquals(varInt1.size, size, @"16bit size1 does not match");
+	
+	const char bytes[] = { 0xfd, 0xff, 0xff };
+	NSData *data = [NSData dataWithBytes:bytes length:sizeof(bytes)];
+	BitcoinVarInt *varInt2 = [[BitcoinVarInt alloc] initFromBytes:data fromOffset:0];
+	STAssertEquals(varInt2.value, value, @"16bit value2 does not match");
+	STAssertEquals(varInt2.size, size, @"16bit size2 does not match");
+	
+	NSData *dataFromVarInt = [varInt1 getData];
+	STAssertEqualObjects(dataFromVarInt, data, @"16bit getData does not match");
 }
 
 @end
