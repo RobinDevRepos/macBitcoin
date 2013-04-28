58101dab8427f4ca6823afb0e9f5a4501f3e7cf3
Myles Grant <myles@mylesgrant.com>
2013-04-28 16:56:49 -0700

    OK, genesis block tx in is confirmed correct

--- MacBitCoin/BitcoinBlock.m
+++ MacBitCoin/BitcoinBlock.m
@@ -49,7 +49,7 @@
 		BitcoinTransaction *tx = [BitcoinTransaction transaction];
 		
 		BitcoinTxIn *txIn = [BitcoinTxIn txIn];
-		NSData *script = [[@"04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73" stringToHexData] reverseBytes];
+		NSData *script = [@"04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73" stringToHexData];
 		[txIn scriptFromBytes:script];
 		[tx addTxIn:txIn];
 		
@@ -60,7 +60,7 @@
 		[pkScript appendData:[[@"04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f" stringToHexData] reverseBytes]];
 		char op_checksig[] = { 0xac };
 		[pkScript appendBytes:op_checksig length:1];
-		NSLog(@"pk script: %@", pkScript);
+		//NSLog(@"pk script: %@", pkScript);
 		[txOut scriptFromBytes:pkScript];
 		[tx addTxOut:txOut];
 		
